551564325ede4cab4f1b90b4919c5ce3ba55820e
sean <sean.shen@propelventures.ai>
2020-10-20 02:43:14 +1100

    ff

--- ops/e2e.sh
+++ ops/e2e.sh
@@ -2,6 +2,7 @@
 echo "Running Cypress e2e tests headlessly"
 docker run -it \
     -v $PWD:/e2e \
+    -v $PWD/cypress/cypress.json:/e2e/cypress.json \
     -v ~/.aws/credentials:/root/.aws/credentials \
     -e CYPRESS_TAX_EMAIL=archietest.user1propel@gmail.com\
     -e CYPRESS_TAX_PASSWORD=Myob1234\
