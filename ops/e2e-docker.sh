b0c88f090fcb54e51cc7fba09cc43b3309880940
sean <sean.shen@propelventures.ai>
2020-10-20 15:00:14 +1100

    wip

---
+++ ops/e2e-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu
+echo "Running Cypress e2e tests headlessly"
+docker run -it \
+    -v $PWD/cypress:/e2e/cypress \
+    -v $PWD/cypress.json:/e2e/cypress.json \
+    -v ~/.aws/credentials:/root/.aws/credentials \
+    -e CYPRESS_TAX_EMAIL=archietest.user1propel@gmail.com\
+    -e CYPRESS_TAX_PASSWORD=Myob1234\
+    -w /e2e cypress/included:5.3.0
--- ops/e2e.sh
+++ ops/e2e.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -eu
 echo "Running Cypress e2e tests headlessly"
-echo $(ls) 
-# buildkite-agent pipeline upload .buildkite/pipeline.a.yml
+# docker run -it \
+#     -v $PWD/cypress:/e2e/cypress \
+#     -v $PWD/cypress.json:/e2e/cypress.json \
+#     -v ~/.aws/credentials:/root/.aws/credentials \
+#     -e CYPRESS_TAX_EMAIL=archietest.user1propel@gmail.com\
+#     -e CYPRESS_TAX_PASSWORD=Myob1234\
+#     -w /e2e cypress/included:5.3.0
 
-docker run -it \
-    -v $PWD/cypress:/e2e/cypress \
-    -v $PWD/cypress.json:/e2e/cypress.json \
-    -v ~/.aws/credentials:/root/.aws/credentials \
-    -e CYPRESS_TAX_EMAIL=archietest.user1propel@gmail.com\
-    -e CYPRESS_TAX_PASSWORD=Myob1234\
-    -w /e2e cypress/included:5.3.0
-
-    
+SSM_NAMESPACE=/bk/taxmanager-dev
+# export MS=$(aws ssm get-parameters --name $SSM_NAMESPACE/shell-auth-myob-clientSecret --with-decryption --query "Parameters[0].Value" --region ap-southeast-2|sed -e 's/\"//g')
+export CYPRESS_TAX_EMAIL=archietest.user1propel@gmail.com
+export CYPRESS_TAX_PASSWORD=Myob1234  
+npm install cypress@5.4.0
+npm run cy:run
