368682929308999d10aef9e233845e143dbd70bf
sean <sean.shen@propelventures.ai>
2020-10-27 14:55:13 +1100

    test development branch

--- cypress/integration/e2e/taxnotice.spec.js
+++ cypress/integration/e2e/taxnotice.spec.js
@@ -7,40 +7,40 @@ describe('tax notice test', function() {
 
   // because dom is modified, cypress needs to re-login. 
   // alternatively, save session. one login can test many senarios. not opt because of complexity 
-  beforeEach(() => {
-    cy.login(practice);
-    cy.server()
-      .route(link.tax_manage_notice_api(clientid))
-      .as('getNotice')
-      .visit(link.tax_manage_notice_page(clientid));
-  });
+  // beforeEach(() => {
+  //   cy.login(practice);
+  //   cy.server()
+  //     .route(link.tax_manage_notice_api(clientid))
+  //     .as('getNotice')
+  //     .visit(link.tax_manage_notice_page(clientid));
+  // });
 
   it('tax notice update', function() {
     // open provisional instalment 2
     // tricky part is table notices dom already exsit, which is not dynamically generated. 
     // wait children appears, then it is safe to click the notice and continue test 
-    cy.get(':nth-child(1) > [title="Customer Seven"]', {timeout: 20000});
-    cy.get('div.table-data__body > div > div:nth-child(2)').click();
+    // cy.get(':nth-child(1) > [title="Customer Seven"]', {timeout: 20000});
+    // cy.get('div.table-data__body > div > div:nth-child(2)').click();
     
-    // standard
-    cy.get('[name="TaxOption"]').select('Standard');
-    cy.get('input[name="TaxCredits"]').clear().type(100.00);
-    cy.get('[name="TaxableIncome"]').should('have.value', 357.14);
+    // // standard
+    // cy.get('[name="TaxOption"]').select('Standard');
+    // cy.get('input[name="TaxCredits"]').clear().type(100.00);
+    // cy.get('[name="TaxableIncome"]').should('have.value', 357.14);
   });
 
   it('tax notice delete and create', function() {
     // cy.login(practice);
-    cy.server()
-      .route(link.tax_manage_notice_api(clientid))
-      .as('getNotice')
-      .visit(link.tax_manage_notice_page(clientid));
-    // purge
-    cy.wait('@getNotice', { timeout: 30000 }).then(xhr => purgeTaxNotice(xhr));
+    // cy.server()
+    //   .route(link.tax_manage_notice_api(clientid))
+    //   .as('getNotice')
+    //   .visit(link.tax_manage_notice_page(clientid));
+    // // purge
+    // cy.wait('@getNotice', { timeout: 30000 }).then(xhr => purgeTaxNotice(xhr));
 
-    // verify creation
-    cy.reload()
-      .get(cssLocator.create_tax_notice, { timeout: 10000 })
-      .contains('Create now')
-      .click();
+    // // verify creation
+    // cy.reload()
+    //   .get(cssLocator.create_tax_notice, { timeout: 10000 })
+    //   .contains('Create now')
+    //   .click();
   });
 });
