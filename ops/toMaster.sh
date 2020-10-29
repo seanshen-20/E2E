cc31449fa59fe31eb908eb9299463acedaee1483
sean <sean.shen@propelventures.ai>
2020-10-29 18:58:50 +1100

    feature-3-882C6D28-C0DA-4F71-B4D0-C4E7DD85EE1A

--- ops/toMaster.sh
+++ ops/toMaster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git checkout master 
-git merge "${1:-development}"; 
+git merge "${1:-development}" --no-ff -m "$(git config user.email) pushed to master"; 
 git push 
 gh pr create -f 
