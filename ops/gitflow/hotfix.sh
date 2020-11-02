49426a1a61ed337a11c719c56fba2d5c47a365dc
sean <sean.shen@propelventures.ai>
2020-11-02 14:31:38 +1100

    fix hotfix.sh

--- ops/gitflow/hotfix.sh
+++ ops/gitflow/hotfix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-git -version &> /dev/null && echo "It will submit pr to master and dev branch. " || (echo "please go to git UI and submit pr to both master and dev branch! "; $SHELL)
+gh &> /dev/null && echo "It will submit pr to master and dev branch. " || (echo "please go to git UI and submit pr to both master and dev branch! "; $SHELL)
 
 gh pr create -f -B master
 gh pr create -f -B development
