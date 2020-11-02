1fe1b358474469ac4bc949d2e20396c656cf303e
sean <seanshen20@gmail.com>
2020-11-02 13:14:58 +1100

    add hotfix.sh

---
+++ ops/gitflow/hotfix.sh
@@ -0,0 +1,11 @@
+git -version &> /dev/null && echo "ok" || (echo "please go to git UI and submit pr! "; $SHELL)
+
+echo "h"
+# if [gh]; then
+#     echo "success"
+# else
+#     echo "please go to git UI and submit pr! "
+# fi
+# echo "h"
+
+$SHELL
