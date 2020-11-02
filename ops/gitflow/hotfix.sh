3918ad67a5bf3b5b01d31024826cca57d4ef7767
sean <seanshen20@gmail.com>
2020-11-02 13:48:32 +1100

    modify hotfix.sh

--- ops/gitflow/hotfix.sh
+++ ops/gitflow/hotfix.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git -version &> /dev/null && echo "ok" || (echo "please go to git UI and submit pr! "; $SHELL)
 
-echo "h"
+
+git -version &> /dev/null && echo "It will submit pr to master and dev branch. " || (echo "please go to git UI and submit pr to both master and dev branch! "; $SHELL)
+
+gh pr create -f -B master
+gh pr create -f -B development
 # if [gh]; then
 #     echo "success"
 # else
